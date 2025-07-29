503aef9c91c0e61efa00e8827f1207934481ef9b
arunci <96084440+arunci@users.noreply.github.com>
2025-07-29 18:48:27 +0200

    create random walks file

---
+++ random_walk.py
@@ -0,0 +1,12 @@
+from random import choice
+
+
+class RandomWalk:
+    """A class to generate randomo walks."""
+
+    def __init__(self, num_points=5000):
+        self.num_points = num_points
+
+        # All walks start at (0, 0).
+        self.x_values = [0]
+        self.y_values = [0]
--- scatter_squares.py
+++ scatter_squares.py
@@ -5,7 +5,7 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use("seaborn-v0_8")
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -17,6 +17,8 @@ ax.tick_params(labelsize=14)
 
 # Set the range for each axis
 ax.axis([0, 1100, 0, 1_100_000])
+ax.ticklabel_format(style="plain")
 
 
-plt.show()
+# plt.show()
+plt.savefig("squares_plot.png", bbox_inches="tight")
